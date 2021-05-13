a5a341b824ff1cc6c23dd28b6b6f42d1f020324c
Suvi <sumaya.shahoyi@gmail.com>
2021-05-13 09:35:50 +0300

    yup

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Suvi Shahoyi',
+      title: 'Sumaya Mohammed',
       debugShowCheckedModeBanner: false,
       home: Home(),
     );
